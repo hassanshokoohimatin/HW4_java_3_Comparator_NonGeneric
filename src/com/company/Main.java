43205b4e0d6534f833c5ee5b6b651af9ae4c25e3
asus <hassan.shokoohimatin@gmail.com>
2019-12-12 01:43:14 +0330

    final commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,32 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        ArrayList al = new ArrayList();
+        al.add(new Student(101, "Vahid", 23));
+        al.add(new Student(106, "Ali", 27));
+        al.add(new Student(105, "Jalal", 21));
+
+        System.out.println("Sorting by Name");
+
+        Collections.sort(al, new NameComparator());
+        for(Object o : al) {
+            System.out.println(o);
+            System.out.println("\t");
+        }
+        System.out.println("\n");
+
+        System.out.println("Sorting by age");
+
+        Collections.sort(al, new AgeComparator());
+        for(Object o : al) {
+            System.out.println(o);
+            System.out.println("\t");
+        }
 
     }
 }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class Student {
+        int rollno;
+        String name;
+        int age;
+        Student(int rollno,String name,int age){
+            this.rollno=rollno;
+            this.name=name;
+            this.age=age;
+        }
+
+    @Override
+    public String toString() {
+        return rollno +
+                ", name= " + name +
+                ", age= " + age ;
+    }
 }
